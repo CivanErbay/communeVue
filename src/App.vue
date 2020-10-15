a195d836d4e9ee9d16cb7b039171c7a7edfef52f
Civan <civan.erbay@web.de>
2020-10-15 23:48:09 +0200

    Add Landing component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Landing/>
+  </div>
+  
 </template>
 
 <script>
 
+import Landing  from './components/Landing.vue'
+
 export default {
   name: 'App',
   components: {
- 
+    Landing
   }
 }
 </script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Sömmering</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Landing"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
