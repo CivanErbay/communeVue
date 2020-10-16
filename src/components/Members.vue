a59d39f5b096b1a97b025087fb6fd1b158df8e27
Civan <civan.erbay@web.de>
2020-10-16 11:47:57 +0200

    Add Member Component

---
+++ src/components/Members.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="member-container">
+        {{message}}
+        <ul class="member-container__ul">
+            <li v-for="member in members" :key="member">{{member}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Members",
+        data: () => ({
+            message: "Unsere aktuellen WG-Mitglieder",
+            members: [
+                "Anthea",
+                "Paul",
+                "Eyleen",
+                "Paco",
+                "Civan"
+            ]
+        })
+    }
+</script>
+
+<style lang="scss" scoped>
+    .member-container {
+        &__ul{
+                list-style-type: none;
+                padding: 0;
+        }
+    }
+</style>
