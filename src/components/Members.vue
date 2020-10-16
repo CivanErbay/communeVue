499b08a1cd7a7f5f6355cebeca21772f422b1b67
Civan <civan.erbay@web.de>
2020-10-16 14:19:23 +0200

    Add Centered Memberlist

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,33 +1,37 @@
 <template>
-    <div class="member-container">
-        {{message}}
-        <ul class="member-container__ul">
-            <li v-for="member in members" :key="member">{{member}}</li>
-        </ul>
-    </div>
+  <div class="member-container">
+    {{ message }}
+    <ul class="member-container__ul">
+      <li v-for="(member, index) in members" :key="member">
+        {{ ++index }} {{ member }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Members",
-        data: () => ({
-            message: "Unsere aktuellen WG-Mitglieder",
-            members: [
-                "Anthea",
-                "Paul",
-                "Eyleen",
-                "Paco",
-                "Civan"
-            ]
-        })
-    }
+export default {
+  name: "Members",
+  data: () => ({
+    message: "Unsere aktuellen WG-Mitglieder",
+    members: ["Anthea", "Paul", "Eyleen", "Paco", "Civan"],
+  }),
+};
 </script>
 
 <style lang="scss" scoped>
-    .member-container {
-        &__ul{
-                list-style-type: none;
-                padding: 0;
-        }
-    }
+.member-container {
+    max-width: 20%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  &__ul {
+    
+    list-style-type: none;
+    padding: 0;
+    
+    text-align: left;
+  }
+}
 </style>
